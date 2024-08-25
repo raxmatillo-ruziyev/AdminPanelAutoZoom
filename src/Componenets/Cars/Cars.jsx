3d48b42d6525f03177f4cdc8c6dfcffbc93b4950
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-08-25 22:34:59 +0500

    add

--- src/Componenets/Cars/Cars.jsx
+++ src/Componenets/Cars/Cars.jsx
@@ -368,7 +368,7 @@ const getData = async () => {
               name="max_people"
               rules={[{ required: true, message: "Please input the max people!" }]}
             >
-              <Input type='number' />
+              <Input type='number' min={1} />
             </Form.Item>
             <Form.Item
               label="Transmission"
